8e2c4be0123ab9ee4cf371a4c4bd5b347b24c216
devesh <deveshnitrr2017@gmail.com>
2020-06-06 22:36:07 +0530

    added Delete-Ninja functionality

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,34 +1,21 @@
 import React from "react";
 
-// const Ninjas = ({ninjas}) => {
-
-const Ninjas = (props) => {
-  const ninjas = props.ninjas;
-  //other way- by destructuring
-  // const { ninjas } = props;
-
-  // by destructuring method
-  // const Ninjas = ({ninjas}) => {
-
+const Ninjas = ({ ninjas, deleteNinja }) => {
   const ninjaList = ninjas.map((ninja) => {
-    if (ninja.age > 24)
-      return (
-        <div className="ninja-list" key={ninja.id}>
-          <div>name: {ninja.name}</div>
-          <div>age: {ninja.age}</div>
-          <div>belt: {ninja.belt}</div>
-        </div>
-      );
-    else return null;
-
-    // using ternary operator
-    // return ninja.age > 24 ? (
-    //   <div className="ninja-list" key={ninja.id}>
-    //     <div>name: {ninja.name}</div>
-    //     <div>age: {ninja.age}</div>
-    //     <div>belt: {ninja.belt}</div>
-    //   </div>
-    // ) : null;
+    return (
+      <div className="ninja-list" key={ninja.id}>
+        <div>name: {ninja.name}</div>
+        <div>age: {ninja.age}</div>
+        <div>belt: {ninja.belt}</div>
+        <button
+          onClick={() => {
+            deleteNinja(ninja.id);
+          }}
+        >
+          Delete Ninja
+        </button>
+      </div>
+    );
   });
 
   return ninjaList;
