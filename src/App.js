6768c7d764bc45733125095bc039982c23c4ff03
devesh <deveshnitrr2017@gmail.com>
2020-06-07 00:26:57 +0530

    added routes to navbar elements

--- src/App.js
+++ src/App.js
@@ -8,9 +8,14 @@ import Contact from "./components/Contact";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,18 +3,20 @@ import React, { Component } from "react";
 class Navbar extends Component {
   render() {
     return (
-      <nav className="teal nav nav-wrapper">
-        <ul className="right">
-          <li>
-            <a href="/home">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-        </ul>
+      <nav className="teal darken-3 nav-wrapper">
+        <div className="container">
+          <ul className="right">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </div>
       </nav>
     );
   }
